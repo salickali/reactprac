8c6dc428b953c1ec6aef2baed41b826dfcabfa22
salickali <mdsalick.ali@gmail.com>
2020-03-11 17:34:22 +0530

    week three assignment added with react redux forms

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,7 +1,140 @@
-import React from 'react';
-import {Card, CardImg ,  Breadcrumb, BreadcrumbItem, CardText, CardBody,CardTitle} from 'reactstrap';
+import React, { Component } from 'react';
+import {Card, CardImg ,Button,  Breadcrumb, Row, Col, ModalHeader, Modal, ModalBody,Form, FormGroup, Label,Input, BreadcrumbItem, CardText, CardBody,CardTitle} from 'reactstrap';
 import {Link} from 'react-router-dom';
+import {Control , LocalForm, Errors} from 'react-redux-form';
 
+
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !(val) || (val.length <= len);
+const minLength = (len) => (val) => val && (val.length >= len);
+const isNumber = (val) => !isNaN(Number(val));
+const validEmail = (val) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
+
+
+class CommentForm extends Component{
+
+    constructor(props){
+        super(props)
+
+        this.state = {
+            isModalOpen: false
+        };
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    toggleModal(){
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+        });
+    }
+    handleSubmit(values) {
+        console.log('Current State is: ' + JSON.stringify(values));
+        alert('Current State is: ' + JSON.stringify(values));
+        // event.preventDefault();
+    }
+
+    render(){
+        return(
+            
+                <div className="container">
+                    <div className="row">
+                    <div className="col-12">
+                    <Button outline onClick={this.toggleModal}><span className="fa fa-pencil"/>&nbsp;&nbsp;  Submit Comments</Button>
+                        </div>  
+                    </div>
+                    
+                    <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                        Login
+                    </ModalHeader>
+                    <ModalBody>
+                        <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+                            <Row className="form-group">
+                                <Label htmlFor="name" md={12}>Author Name</Label>
+                                <Col md={12}>
+                                    <Control.text model=".name" id="name" name="name"
+                                        placeholder="Author Name"
+                                        className="form-control"
+                                        validators={{
+                                            required, minLength: minLength(3), maxLength: maxLength(15)
+                                        }}
+                                         />
+                                    <Errors
+                                        className="text-danger"
+                                        model=".name"
+                                        show="touched"
+                                        messages={{
+                                            required: 'Required',
+                                            minLength: 'Must be greater than 2 characters',
+                                            maxLength: 'Must be 15 characters or less'
+                                        }}
+                                     />
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Label htmlFor="rating" md={12}>Ratings</Label>
+                                <Col md={12}>
+                                    <Control.input  model=".rating" id="rating" name="rating"
+                                        className="form-control"
+                                        type="number"
+                                        
+                                        validators={{
+                                            required, 
+                                        }}
+                                         />
+                                       
+                                       
+
+                                    <Errors
+                                        className="text-danger"
+                                        model=".rating"
+                                        show="touched"
+                                        messages={{
+                                            required: 'Required',
+                                        }}
+                                     />
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Label htmlFor="comment" md={12}>Comment</Label>
+                                <Col md={12}>
+                                    <Control.textarea model=".comment" id="comment" name="comment"
+                                        placeholder="Enter your Comment"
+                                        className="form-control"
+                                        rows={6}
+                                        validators={{
+                                            required, minLength: minLength(10), maxLength: maxLength(150)
+                                        }}
+                                         />
+                                    <Errors
+                                        className="text-danger"
+                                        model=".comment"
+                                        show="touched"
+                                        messages={{
+                                            required: 'Required',
+                                            minLength: 'Must be greater than 10 characters',
+                                            maxLength: 'Must be 150 characters or less'
+                                        }}
+                                     />
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Col md={{size:12}}>
+                                    <Button type="submit" color="primary">
+                                   Post Comment
+                                    </Button>
+                                </Col>
+                            </Row>
+                            </LocalForm>
+                            </ModalBody>
+                            </Modal>
+                   
+                </div>
+        );
+    }
+
+}
 function RenderDish({dish}){
     return(
        
@@ -66,6 +199,7 @@ const DishDetail = (props) =>{
                 </div>
                 <div className="col-12 col-md-5 m-1">
                     <RenderComments comments={props.comments} />
+                    <CommentForm/>
                 </div>
             </div>
             </div>
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,51 +2,48 @@ import React, { Component } from 'react';
 import Menu from './MenuComponent'
 import Home from './HomeComponent';
 import DishDetail from './DishDetailComponent';
-import {DISHES} from '../shared/dishes'
-import {COMMENTS} from '../shared/comments';
-import {PROMOTIONS} from '../shared/promotions';
-import {LEADERS} from '../shared/leaders';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Contact from './ContactComponent';
-import {Switch, Route , Redirect} from 'react-router-dom';
+import {Switch, Route , Redirect, withRouter} from 'react-router-dom';
 import About from './AboutComponent';
+import {connect} from 'react-redux';
 
+const mapStateToProps = state =>{
+    return{
+      dishes : state.dishes,
+      comments : state.comments,
+      promotions : setImmediate.promotions,
+      leaders : state.leaders
 
+    }
+}
 class Main extends Component{
   constructor(props){
     super(props);
 
-    this.state ={
-      dishes : DISHES,
-      comments : COMMENTS,
-      promotions : PROMOTIONS,
-      leaders : LEADERS,
-      // selectedDish : null
-    };
-  }
-  // onDishSelect(dishId){
-  //   this.setState({selectedDish: dishId});
-  // }
- 
   
+  }
 
 render(){
 
-  /// method 1 for defining the functional component in router path
+  // method 1 for defining the functional component in router path
   const  Homepage =()=>{
     return(
-      <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
-      promotion={this.state.promotions.filter((promo) => promo.featured)[0]}     
-      leader={this.state.leaders.filter((leader) => leader.featured)[0]}
-      />
+      <Home 
+      dish={this.props.dishes.filter((dish) => dish.featured)[0]}
+      promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
+      leader={this.props.leaders.filter((leader) => leader.featured)[0]}
+  />
     );
-  }
+  };
 
   const DishwithId =({match})=>{
     return(
-      <DishDetail dish={this.state.dishes.filter((dish)=> dish.dishId === parseInt(match.params.dishId,10))}
-       comments={this.state.comments.filter((comment)=> comment.dishId === parseInt(match.params.dishId,10))}></DishDetail>
+      <DishDetail 
+      dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+      comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+    
     );
 
   };
@@ -54,10 +51,10 @@ render(){
     <div>
       <Header/>
       <Switch>
-        <Route path="/home" component={Homepage}/>
+        <Route path="/home" component={Homepage} />
 
         {/* /// method two of defining functional component */}
-        <Route exact path="/menu" component={()=> <Menu dishes={ this.state.dishes} />}/>
+        <Route exact path="/menu" component={()=> <Menu dishes={ this.props.dishes} />}/>
 
         <Route path='/menu/:dishId' component={DishwithId}/>
 
@@ -65,7 +62,7 @@ render(){
 
         <Route exact path="/contactus" component={Contact}/>
 
-        <Route path="/about" component ={()=><About leader={this.state.leaders}/>}/>
+        <Route path="/about" component ={()=><About leader={this.props.leaders}/>}/>
 
         <Redirect to="/home"/>
       </Switch>
@@ -76,4 +73,4 @@ render(){
 }
 }
 
-export default Main;
+export default withRouter(connect(mapStateToProps)(Main));
